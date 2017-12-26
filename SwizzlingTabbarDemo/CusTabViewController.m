6793ee6095060c16cddeb76e181978de34ab3238
hzcly98 <hzcly982015@163.com>
2017-12-26 15:06:30 +0800

    再次提交

--- SwizzlingTabbarDemo/CusTabViewController.m
+++ SwizzlingTabbarDemo/CusTabViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "CusTabViewController.h"
+#import "ViewController.h"
+
+#define RANDOM_COLOR [UIColor colorWithHue: (arc4random() % 256 / 256.0) saturation:((arc4random()% 128 / 256.0 ) + 0.5) brightness:(( arc4random() % 128 / 256.0 ) + 0.5) alpha:1]
 
 @interface CusTabViewController ()
 
@@ -17,6 +20,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self setupTabBarChildController];
+}
+- (void)setupTabBarChildController {
+    
+    NSArray *imageArray = @[@"tab_home", @"tab_tz", @"tiger", @"tab_home", @"tab_tz"];
+    NSArray *titleArray = @[@"首页", @"理财", @"", @"发现", @"我的"];
+    
+    for (int i = 0; i < imageArray.count; i++) {
+        ViewController *vc = [ViewController new];
+        [vc.view setBackgroundColor:RANDOM_COLOR];
+        
+        vc.tabBarItem.image = [[UIImage imageNamed:imageArray[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        vc.tabBarItem.selectedImage = [[UIImage imageNamed:[imageArray[i] stringByAppendingString:@"_sel"]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        vc.title = titleArray[i];
+        
+//                if (i == 3) vc.tabBarItem.badgeValue = @"99";
+        UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+        [self addChildViewController:nav];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ SwizzlingTabbarDemo/UITabBar+Swizzling.h
@@ -0,0 +1,15 @@
+//
+//  UITabBar+Swizzling.h
+//  XYTabBar
+//
+//  Created by 潘显跃 on 16/4/5.
+//  Copyright © 2016年 anjuke inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSUInteger const kTabBarBadgeViewDelta;
+
+@interface UITabBar (Swizzling)
+
+@end
